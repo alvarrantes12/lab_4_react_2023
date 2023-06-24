627401b8129da525b627b9beced852d7cecd0243
KendallC09 <kendallconejo00@gmail.com>
2023-06-24 12:03:35 -0600

    Front end Done

---
+++ src/commons/List.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const List = ({ contents }) => {
+  return(
+    <div>
+      <h4>Productos Disponibles</h4>
+      <table className="list">
+        <tr>
+          <th style={{padding: "0 4em 0 4em"}}>Id</th>
+          <th style={{padding: "0 4em 0 4em"}}>Nombre</th>
+          <th style={{padding: "0 4em 0 4em"}}>Precio</th>
+        </tr>
+
+        {contents.map(({id, name, price}) => (
+          <tr>
+            <td style={{padding: "0 4em 0 4em"}}>{id}</td>
+            <td style={{padding: "0 4em 0 4em"}}>{name}</td>
+            <td style={{padding: "0 4em 0 4em"}}>{price}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
+}
+
+export default List;
